8cfc49d73e1ef69b03dd1708d9b2db5140a1c44e
shouta0715 <kshouta0715@gmail.com>
2025-02-01 15:29:24 +0900

    Initial Commit

---
+++ LibraryOfCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LibraryOfCode
+//
+//  Created by k21047kk on 2025/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LibraryOfCode/LibraryOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryOfCodeApp.swift
+//  LibraryOfCode
+//
+//  Created by k21047kk on 2025/02/01.
+//
+
+import SwiftUI
+
+@main
+struct LibraryOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
