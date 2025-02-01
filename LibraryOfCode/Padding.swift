ec5802818b2de91956bc4b7adfce94669dabd40a
shouta0715 <kshouta0715@gmail.com>
2025-02-01 16:13:58 +0900

    padding

--- LibraryOfCode/Padding.swift
+++ LibraryOfCode/Padding.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct Padding: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Text("Padding")
+          .padding(20)  // 文字の周りに余白を追加
+          
+      Text("Edge Insets")
+        .padding(EdgeInsets(top: 100, leading: 40, bottom: 20, trailing: 33))
+        .background(Color.blue)
+        .foregroundStyle(.white)
+      
+      Text("Leading and Top")
+        .padding([.top, .leading], 20)
+        .background(Color.red)
+        
+      
     }
 }
 
