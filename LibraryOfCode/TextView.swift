73e1c7a4627d1914ddb337503d4e34f3837d3164
shouta0715 <kshouta0715@gmail.com>
2025-02-01 15:58:24 +0900

    Text View

---
+++ LibraryOfCode/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  LibraryOfCode
+//
+//  Created by k21047kk on 2025/02/01.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextView()
+}
