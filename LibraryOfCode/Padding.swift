2dbb6e38ab4d1add0254b47d25e1de22c42a76a5
shouta0715 <kshouta0715@gmail.com>
2025-02-01 16:13:39 +0900

    padding

---
+++ LibraryOfCode/Padding.swift
@@ -0,0 +1,18 @@
+//
+//  Padding.swift
+//  LibraryOfCode
+//
+//  Created by k21047kk on 2025/02/01.
+//
+
+import SwiftUI
+
+struct Padding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Padding()
+}
